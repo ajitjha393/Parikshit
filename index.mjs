0f90a6ded6ad6cf409aba55b117f9aebc087693b
ajitjha393 <ajitjha393@gmail.com>
2022-08-17 16:28:54 +0530

    `feat:` create hasteMap for getting all files

--- index.mjs
+++
@@ -1,5 +0,0 @@
-import glob from 'glob'
-
-const testFiles = glob.sync('**/*.test.js')
-
-console.log(testFiles) // ['tests/01.test.js', 'tests/02.test.js', …]WWW
---
+++ index.ts
@@ -0,0 +1,21 @@
+﻿import HasteMap from 'jest-haste-map'
+import {cpus} from 'os'
+
+const root = __dirname
+
+const hasteMap = HasteMap.create({
+    rootDir: root,
+    roots: [root],
+    maxWorkers: cpus.length,
+    platforms: [],
+    extensions: ['js'],
+    name: 'parikshit-test-framework',
+    retainAllFiles: false
+});
+
+
+(async() => {
+    const { hasteFS } = await hasteMap.build()
+    console.log(hasteFS.getAllFiles())
+})()
+
