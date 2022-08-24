fc5294367ed94454a3904d0eebab008d32a95660
ajitjha393 <ajitjha393@gmail.com>
2022-08-24 20:30:53 +0530

    `feat:` add describe and it tests

---
+++ tests/describe.test.js
@@ -0,0 +1,11 @@
+﻿describe('Describe test', () => {
+  it('this should work', () => {
+    expect(1).toBe(1)
+  })
+})
+
+describe('Second Describe test', () => {
+  it(`this should fail`, () => {
+    expect(1).toBe(2)
+  })
+})
