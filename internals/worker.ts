173460f0f920fc128abe6ed6dc972b72afb8706b
ajitjha393 <ajitjha393@gmail.com>
2022-08-24 20:30:20 +0530

    `feat:` implement describe and it test runner

--- internals/worker.ts
+++ internals/worker.ts
@@ -10,6 +10,13 @@ type TestResult = {
     errorMessage: Nullable<string> 
 }
 
+type Runner = {
+    name: string,
+    fn: () => void
+}
+
+type RunnerTuple = [Runner['name'], Runner['fn']]
+
 const expect = _expect
 
 const materializeImport = () => {
@@ -19,16 +26,40 @@ const materializeImport = () => {
 
 export const runTest = async (testFile: string): Promise<TestResult> => {
     const code = await fs.promises.readFile(testFile, 'utf-8')
-    const testResult = {
+    const testResult: TestResult = {
         success: false,
         errorMessage: null
     }
 
+    let testName = ''
+    
     try{
+        const describeFns: RunnerTuple[] = []
+        let currentDescribeFn: RunnerTuple[] = []
+
+        const describe = (name: Runner['name'], fn: Runner['fn']) => {
+            describeFns.push([name, fn])
+        }
+        const it = (name: Runner['name'], fn: Runner['fn']) => {
+            currentDescribeFn.push([name, fn])
+        }
+
         eval(code)
+
+        for(const [name, fn] of describeFns) {
+            currentDescribeFn = []
+            testName = name
+            fn()
+
+            for(const [itName, itFn] of currentDescribeFn){
+                testName += ' ' + itName  
+                itFn()
+            }
+        }
+
         testResult.success = true
     }catch(error){
-        testResult.errorMessage = (error as any).message
+        testResult.errorMessage = (testName + ':' +  (error as any).message)
     }
 
     return testResult
