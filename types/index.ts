9c3c4141e9f1bc9515661d059fb14adf2a1c0102
ajitjha393 <ajitjha393@gmail.com>
2022-08-25 09:26:34 +0530

    `chore: ` move type from source

---
+++ types/index.ts
@@ -0,0 +1,14 @@
+﻿export type Nullable<T> = T | null
+
+export type TestResult = {
+    success: boolean,
+    errorMessage: Nullable<string> 
+}
+
+export type Runner = {
+    name: string,
+    fn: () => void
+}
+
+export type RunnerTuple = [Runner['name'], Runner['fn']]
+
