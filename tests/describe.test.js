99df646f687c9591a756e2e30e761c224f8c6cc0
ajitjha393 <ajitjha393@gmail.com>
2022-08-26 22:50:50 +0530

    `test`: add a test to check async code flow

--- tests/describe.test.js
+++ tests/describe.test.js
@@ -5,7 +5,8 @@
 })
 
 describe('Second Describe test', () => {
-  it(`this should fail`, () => {
+  it(`Test for checking async code`, async () => {
+    await new Promise(resolve => setTimeout(resolve, 200))
     expect(1).toBe(2)
   })
 })
