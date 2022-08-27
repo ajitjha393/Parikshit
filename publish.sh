e71cb94fcd9600404e936f1943b343b32425245a
ajitjha393 <ajitjha393@gmail.com>
2022-08-27 18:00:04 +0530

    `publish`: add npm publish script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+﻿
+rm -rf dist || true
+npm run build
+mkdir dist
+cp index.js dist/index.js
+cp README.md dist/README.md
+cp -r internals dist/
+cp package.json dist/package.json
+rm -rf dist/internals/*.ts
+npm publish
+git tag $(cat package.json | grep version | sed -r 's/.*"version": "(.*)".*/v\1/')
