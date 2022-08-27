b9abffae2263209c936173f990048841bc2c7c4e
ajitjha393 <ajitjha393@gmail.com>
2022-08-27 11:33:06 +0530

    `feat`: add custom require for import. closes #10

--- internals/worker.ts
+++ internals/worker.ts
@@ -4,7 +4,9 @@ import expect from 'expect'
 import { describe, it, run, resetState}  from 'jest-circus'
 import vm from 'vm'
 import NodeEnvironment from 'jest-environment-node'
+import { join, dirname } from 'path'
 import { TestResult } from '../types'
+
 
 export const runTest = async (testFile: string): Promise<TestResult> => {
     const code = await fs.promises.readFile(testFile, 'utf-8')
@@ -14,10 +16,22 @@ export const runTest = async (testFile: string): Promise<TestResult> => {
         errorMessage: null
     }
 
-    let testName = ''
     
     try{
-        const _context = { describe, it, mock, expect }
+        const customRequire = (fileName: string): any => {
+            const code = fs.readFileSync(join(dirname(testFile), fileName), 'utf8')
+            
+            const module = { exports: {} }
+            const moduleFactory =  vm.runInContext(
+                `(function(module) { ${code} })` 
+                , environment.getVmContext()!
+                )
+            
+            moduleFactory(module)
+            return module.exports    
+        }
+ 
+        const _context = { describe, it, mock, expect, require: customRequire }
         const environment = new NodeEnvironment({
             // @ts-ignore
             projectConfig: { 
@@ -32,7 +46,7 @@ export const runTest = async (testFile: string): Promise<TestResult> => {
         testResult.testResults = testResults
         testResult.success = testResults.every(result => !result.errors.length)
     }catch(error){
-        testResult.errorMessage = (testName + ':' +  (error as any).message)
+        testResult.errorMessage = (error as any).message
     }
 
     return testResult
